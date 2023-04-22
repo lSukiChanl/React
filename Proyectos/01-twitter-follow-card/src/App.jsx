2b2781ec70c74bd2150f9eb37a28654375a285b9
SukiChan <crtmg@outlook.com>
2023-04-22 16:58:10 -0600

    Proyecto 03 Terminado

---
+++ Proyectos/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,9 @@
+function App () {
+  return (
+    <>
+      <h1>Twitter Follow Card</h1>
+    </>
+  )
+}
+
+export default App
--- Proyectos/03-siguiendo-mouse/src/App.jsx
+++ Proyectos/03-siguiendo-mouse/src/App.jsx
@@ -1,7 +1,55 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+const FollowMouse = () => {
+  const [Activado, setActivado] = useState(false)
+  const [Posicion, setPosicion] = useState({ x: 0, y: 0 })
+  useEffect(() => {
+    console.log('Efecto', { Activado })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosicion({ x: clientX, y: clientY })
+    }
+
+    if (Activado) window.addEventListener('pointermove', handleMove)
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [Activado])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '2px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${Posicion.x}px, ${Posicion.y}px)`
+      }}
+      />
+      <button onClick={() => setActivado(!Activado)}>
+        {Activado ? 'Desactivar' : 'Activar'}
+      </button>
+    </main>
+  )
+}
+
 function App () {
+  const [mount, setMount] = useState(false)
   return (
     <>
-      <h1>Seguimiento del mouse</h1>
+      {mount && <FollowMouse />}
+      <button onClick={() => setMount(!mount)}>
+        {mount ? 'Desmontar' : 'Montar'}
+      </button>
     </>
   )
 }
