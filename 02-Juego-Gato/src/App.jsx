5d5de40921b2f83242257baf64cc6dd6af3e6a55
SukiChan <crtmg@outlook.com>
2023-09-15 17:20:57 -0600

    Cambio de Turno y Dibujar Tablero

--- 02-Juego-Gato/src/App.jsx
+++ 02-Juego-Gato/src/App.jsx
@@ -1,33 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+const Turnos = {
+  player1: '✘',
+  player2: '◯'
+}
+
+const Ganadores = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+// eslint-disable-next-line react/prop-types
+const Cuadrado = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`	
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+  return (
+    <div className={className} onClick={handleClick}>
+      {children}
+    </div>
+  )
+}
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Tablero, setTablero] = useState(Array(9).fill(null))
+  const [Turno, setTurno] = useState(Turnos.player1)
+  const [Ganador, setGanador] = useState(null)
+
+  const updateBoard = (index) => {
+
+    if (Tablero[index]) return
+
+    const NewTurno = Turno === Turnos.player1 ? Turnos.player2 : Turnos.player1
+    setTurno(NewTurno);
+
+    const NewTablero = [...Tablero]
+    NewTablero[index] = Turno
+    setTablero(NewTablero)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="board">
+        <h1 className="title">Juego del Gato</h1>
+        <section className="game">
+          {
+            Tablero.map((_, index) => {
+              return (
+                <Cuadrado key={index} index={index} updateBoard={updateBoard}> {Tablero[index]} </Cuadrado>
+              )
+            })
+          }
+        </section>
+
+        <section className="turn">
+          <Cuadrado isSelected={Turno === Turnos.player1} > {Turnos.player1} </Cuadrado>
+          <Cuadrado isSelected={Turno === Turnos.player2} > {Turnos.player2} </Cuadrado>
+        </section>
+        
+      </main>
+
     </>
   )
 }
