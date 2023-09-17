b5a6f25e3991e3b8e476a4daa6cda739d4790e02
SukiChan <crtmg@outlook.com>
2023-09-16 21:28:51 -0600

    Juego del Gato Terminado

--- 02-Juego-Gato/src/App.jsx
+++ 02-Juego-Gato/src/App.jsx
@@ -5,7 +5,7 @@ const Turnos = {
   player2: '◯'
 }
 
-const Ganadores = [
+const ListaGanadores = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -42,6 +42,7 @@ function App() {
   const updateBoard = (index) => {
 
     if (Tablero[index]) return
+    if (Ganador) return
 
     const NewTurno = Turno === Turnos.player1 ? Turnos.player2 : Turnos.player1
     setTurno(NewTurno);
@@ -49,6 +50,34 @@ function App() {
     const NewTablero = [...Tablero]
     NewTablero[index] = Turno
     setTablero(NewTablero)
+
+    const newGanador = revisarGanador(NewTablero)
+    if (newGanador) {
+      setGanador(newGanador)
+    } else if (revisarFinal(NewTablero)) {
+      setGanador(false)
+    }
+
+  }
+
+  const revisarGanador = (NewTablero) => {
+    for(const Combinacion of ListaGanadores) { 
+      const [a, b, c] = Combinacion
+      if (NewTablero[a] && NewTablero[a] === NewTablero[b] && NewTablero[a] === NewTablero[c]) {
+        return NewTablero[a]
+      }
+    }
+    return null
+  }
+
+  const revisarFinal = (NewTablero) => {
+    return NewTablero.every((Casilla) => Casilla != null)
+  }
+
+  const resetGame = () => {
+    setTablero(Array(9).fill(null))
+    setTurno(Turnos.player1)
+    setGanador(null)
   }
 
   return (
@@ -70,6 +99,27 @@ function App() {
           <Cuadrado isSelected={Turno === Turnos.player2} > {Turnos.player2} </Cuadrado>
         </section>
         
+        {
+          Ganador != null && (
+            <section className="winner">
+              <h2 className="text">
+                {
+                  Ganador === false ? 'Empate' : `Ganador:`
+                }
+
+                <header className="win">
+                  {
+                    Ganador && <Cuadrado> {Ganador} </Cuadrado>
+                  }
+                </header>
+
+                <footer className="reset">
+                  <button className="button" onClick={resetGame}>Reiniciar Juego</button>
+                </footer>
+              </h2>
+            </section>
+          )
+        }
       </main>
 
     </>
