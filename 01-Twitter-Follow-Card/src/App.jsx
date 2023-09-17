29d2ad52c61415ee9d5906c56dc20ddc2d8482a9
SukiChan <crtmg@outlook.com>
2023-09-17 02:28:58 -0600

    Separando el Componente

--- 01-Twitter-Follow-Card/src/App.jsx
+++ 01-Twitter-Follow-Card/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
+import { TwitterFollowCard } from "./assets/TwitterFollowCard.jsx";
  
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <div>
         <TwitterFollowCard userName='@SukiChan' isFollowing={false} formatUserName={userName => userName.toUpperCase()} avatar={'https://avatars.githubusercontent.com/u/56332696'}> Cristian Mendez  Galicia </TwitterFollowCard>
         <br></br>
-        <TwitterFollowCard userName='@FrancescaP' isFollowing={true} formatUserName={userName => userName.toUpperCase()} avatar={'https://avatars.githubusercontent.com/u/15771072'}> Francesca Prelati </TwitterFollowCard>
+        <TwitterFollowCard userName='@FP' isFollowing={true} formatUserName={userName => userName.toUpperCase()} avatar={'https://avatars.githubusercontent.com/u/15771072'}> Suki-Chan </TwitterFollowCard>
       </div>
     </>
   )
