3e80dc37688e3d8442a1cde34386470dfb3f231a
SukiChan <crtmg@outlook.com>
2023-09-17 04:48:20 -0600

    Cambio nombre variables y metodos a ingles

--- 02-Juego-Gato/src/App.jsx
+++ 02-Juego-Gato/src/App.jsx
@@ -1,24 +1,11 @@
 import { useState } from "react"
 import confetti from 'canvas-confetti'
 
-const Turnos = {
-  player1: '✘',
-  player2: '◯'
-}
-
-const ListaGanadores = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+import { Turns } from "./constants/constans"
+import { CombosWinners } from "./constants/constans"
 
 // eslint-disable-next-line react/prop-types
-const Cuadrado = ({ children, isSelected, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`	
 
   const handleClick = () => {
@@ -32,54 +19,51 @@ const Cuadrado = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
-
-
-
 function App() {
-  const [Tablero, setTablero] = useState(Array(9).fill(null))
-  const [Turno, setTurno] = useState(Turnos.player1)
-  const [Ganador, setGanador] = useState(null)
+  const [Board, setBoard] = useState(Array(9).fill(null))
+  const [Turn, setTurn] = useState(Turns.player1)
+  const [Winner, setWinner] = useState(null)
 
   const updateBoard = (index) => {
 
-    if (Tablero[index]) return
-    if (Ganador) return
+    if (Board[index]) return
+    if (Winner) return
 
-    const NewTurno = Turno === Turnos.player1 ? Turnos.player2 : Turnos.player1
-    setTurno(NewTurno);
+    const NewTurn = Turn === Turns.player1 ? Turns.player2 : Turns.player1
+    setTurn(NewTurn);
 
-    const NewTablero = [...Tablero]
-    NewTablero[index] = Turno
-    setTablero(NewTablero)
+    const NewBoard = [...Board]
+    NewBoard[index] = Turn
+    setBoard(NewBoard)
 
-    const newGanador = revisarGanador(NewTablero)
-    if (newGanador) {
+    const newWinner = checkWinner(NewBoard)
+    if (newWinner) {
       confetti()
-      setGanador(newGanador)
-    } else if (revisarFinal(NewTablero)) {
-      setGanador(false)
+      setWinner(newWinner)
+    } else if (checkEnd(NewBoard)) {
+      setWinner(false)
     }
 
   }
 
-  const revisarGanador = (NewTablero) => {
-    for(const Combinacion of ListaGanadores) { 
+  const checkWinner = (NewBoard) => {
+    for(const Combinacion of CombosWinners) { 
       const [a, b, c] = Combinacion
-      if (NewTablero[a] && NewTablero[a] === NewTablero[b] && NewTablero[a] === NewTablero[c]) {
-        return NewTablero[a]
+      if (NewBoard[a] && NewBoard[a] === NewBoard[b] && NewBoard[a] === NewBoard[c]) {
+        return NewBoard[a]
       }
     }
     return null
   }
 
-  const revisarFinal = (NewTablero) => {
-    return NewTablero.every((Casilla) => Casilla != null)
+  const checkEnd = (NewBoard) => {
+    return NewBoard.every((Casilla) => Casilla != null)
   }
 
   const resetGame = () => {
-    setTablero(Array(9).fill(null))
-    setTurno(Turnos.player1)
-    setGanador(null)
+    setBoard(Array(9).fill(null))
+    setTurn(Turns.player1)
+    setWinner(null)
   }
 
   return (
@@ -88,30 +72,30 @@ function App() {
         <h1 className="title">Juego del Gato</h1>
         <section className="game">
           {
-            Tablero.map((_, index) => {
+            Board.map((_, index) => {
               return (
-                <Cuadrado key={index} index={index} updateBoard={updateBoard}> {Tablero[index]} </Cuadrado>
+                <Square key={index} index={index} updateBoard={updateBoard}> {Board[index]} </Square>
               )
             })
           }
         </section>
 
         <section className="turn">
-          <Cuadrado isSelected={Turno === Turnos.player1} > {Turnos.player1} </Cuadrado>
-          <Cuadrado isSelected={Turno === Turnos.player2} > {Turnos.player2} </Cuadrado>
+          <Square isSelected={Turn === Turn.player1} > {Turn.player1} </Square>
+          <Square isSelected={Turn === Turn.player2} > {Turn.player2} </Square>
         </section>
         
         {
-          Ganador != null && (
+          Winner != null && (
             <section className="winner">
               <h2 className="text">
                 {
-                  Ganador === false ? 'Empate' : `Ganador:`
+                  Winner === false ? 'Empate' : `Ganador:`
                 }
 
                 <header className="win">
                   {
-                    Ganador && <Cuadrado> {Ganador} </Cuadrado>
+                    Winner && <Square> {Winner} </Square>
                   }
                 </header>
 
