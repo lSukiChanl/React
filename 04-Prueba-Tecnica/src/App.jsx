cadbd3684490c74c1ddf465c5eb76c89bedd6722
SukiChan <crtmg@outlook.com>
2023-09-18 15:20:16 -0600

    fetch fact

--- 04-Prueba-Tecnica/src/App.jsx
+++ 04-Prueba-Tecnica/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [Fact, setFact] = useState()
+  const ApiFact = 'https://catfact.ninja/fact'
+  //const ApiCat = 'https://cataas.com/cat/says/hello'
+  //const ApiCatJSON = `https://cataas.com/cat/says/${FirstWord}?json=true`
+
+  useEffect(() => {
+    fetch(ApiFact)
+      .then(response => response.json())
+      .then(data => {
+        const { NewFact } = data
+        setFact(NewFact)
+        const FirstWord = NewFact.split(' ').slice(0, 3).join(' ');
+        console.log(FirstWord);
+      })
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h4>Prueba Tecnica</h4>
+      {Fact && <p>{Fact}</p>}
+      <button > Generar Nuevo </button>
+    </main>
   )
 }
 
