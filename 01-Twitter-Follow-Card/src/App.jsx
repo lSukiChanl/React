0b5f05096da2b1fbd8d53b52657740d00bda5771
SukiChan <crtmg@outlook.com>
2023-09-24 13:03:45 -0600

    Usando recomendaciones inter

--- 01-Twitter-Follow-Card/src/App.jsx
+++ 01-Twitter-Follow-Card/src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
-import { TwitterFollowCard } from "./Components/TwitterFollowCard.jsx";
- 
+import { TwitterFollowCard } from './Components/TwitterFollowCard.jsx'
 
-function App() {
-
+function App () {
   return (
     <>
       <div>
-        <TwitterFollowCard userName='@SukiChan' isFollowing={false} formatUserName={userName => userName.toUpperCase()} avatar={'https://avatars.githubusercontent.com/u/56332696'}> Cristian Mendez  Galicia </TwitterFollowCard>
-        <br></br>
-        <TwitterFollowCard userName='@FP' isFollowing={true} formatUserName={userName => userName.toUpperCase()} avatar={'https://avatars.githubusercontent.com/u/15771072'}> Suki-Chan </TwitterFollowCard>
+        <TwitterFollowCard userName='@SukiChan' isFollowing={false} formatUserName={userName => userName.toUpperCase()} avatar='https://avatars.githubusercontent.com/u/56332696'> Cristian Mendez  Galicia </TwitterFollowCard>
+        <br />
+        <TwitterFollowCard userName='@FP' isFollowing formatUserName={userName => userName.toUpperCase()} avatar='https://avatars.githubusercontent.com/u/15771072'> Suki-Chan </TwitterFollowCard>
       </div>
     </>
   )
